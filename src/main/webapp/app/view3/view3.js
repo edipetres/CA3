4bbf64ae634e1dddf8ff822a637605b831498ac0
matyasfeher <fehermatyas17@gmail.com>
2016-11-02 14:35:23 +0100

    error handeling work with the company

--- src/main/webapp/app/view3/view3.js
+++ src/main/webapp/app/view3/view3.js
@@ -10,19 +10,47 @@ angular.module('myApp.view3', ['ngRoute'])
             }])
 
         .controller('View3Ctrl', function ($http, $scope) {
+            $scope.isError = false;
             $scope.getCompany = function () {
 
                 $http({
                     method: 'GET',
-                    skipAuthorization : true,
+                    skipAuthorization: true,
                     url: 'http://cvrapi.dk/api?vat=' + $scope.cvr + '&country=dk'
-                    
+
                 }).then(function successCallback(res) {
                     $scope.data = res.data;
-                    
+                    checkForGETRequestError();
+
                 }, function errorCallback(res) {
-                    $scope.error = res.status + ": " + res.data.statusText;
+
                 });
             };
 
+            var checkForGETRequestError = function () {
+                if ($scope.data.error === "QUOTA_EXCEEDED") {
+                    $scope.errorMessage = "Your IP address or IP range has been temporarily restricted - usually until the next day. Stop posting. You have 200 free rounds per day.";
+                    $scope.isError = true;
+                }
+                if ($scope.data.error === "BANNED") {
+                    $scope.errorMessage = "Your IP address or IP range is blocked. Stop posting.";
+                    $scope.isError = true;
+                }
+                if ($scope.data.error === "INVALID_VAT") {
+                    $scope.errorMessage = "The VAT number you have turned up is not in the correct format.";
+                    $scope.isError = true;
+                }
+                if ($scope.data.error === "NOT_FOUND") {
+                    $scope.errorMessage = "Found something useful to deliver to you.";
+                    $scope.isError = true;
+                }
+                if ($scope.data.error === "INTERNAL_ERROR") {
+                    $scope.errorMessage = "There has been an error in either the call or the logging of the call. Cabinet finally contact.";
+                    $scope.isError = true;
+                }
+                if ($scope.data.error === "INVALID_UA") {
+                    $scope.errorMessage = "Please do not use the default useragent. Use useragent template that you can find here.";
+                    $scope.isError = true;
+                }
+            };
         });

a5b67e76158314a1f8e68c5c4c625b60d9400668
matyasfeher <fehermatyas17@gmail.com>
2016-11-05 18:57:11 +0100

    view 4 done!!!!!!!

--- src/main/webapp/app/view4/view4.js
+++ src/main/webapp/app/view4/view4.js
@@ -10,21 +10,31 @@ angular.module('myApp.view4', ['ngRoute'])
                 });
             }])
         .controller('View4Ctrl', function ($scope, $http) {
-            $scope.rates = [];
-    
-            $scope.getDailyRates = function () {
+            $scope.$on('$viewContentLoaded', function () {
                 $http({
                     method: 'GET',
                     url: 'api/currency/dailyrates'
                 }).then(function successCallback(res) {
-                    $scope.rates = res.data;
+                    console.log(res.data);
+                    $scope.response = res.data.map.currencies.myArrayList;
+                    console.log($scope.response);
                 }, function errorCallback(res) {
                     $scope.error = res.status + ": " + res.data.statusText;
                 });
-            };
+
+            });
 
             $scope.convert = function () {
 
+                $http({
+                    method: 'GET',
+                    url: 'api/currency/calculator/' + $scope.selectedCurrency + '/' + $scope.fromAmount + '/' + $scope.desiredCurrency
+                }).then(function successCallback(res) {
+                    $scope.result = res;
+                }, function errorCallback(res) {
+                    $scope.error = res.status + ": " + res.data.statusText;
+
+                });
             };
 
         });
